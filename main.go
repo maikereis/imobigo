64b7e07611a325585f5690e5b6408a845ebf1e1f
Maike <maikerdralcantara@gmail.com>
2026-02-18 10:15:40 -0300

    feat: add generateContext function

--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"math/rand"
+	"time"
 	"log"
 	"fmt"
 	"github.com/playwright-community/playwright-go"
 )
 
 const URL = "https://www.vivareal.com.br/aluguel/rio-grande-do-sul/porto-alegre/casa_residencial/"
+
+func RandomChoice[T any](r *rand.Rand, items []T) T{
+	return items[r.Intn(len(items))]
+}
+
+func generateContext(browser playwright.Browser) (playwright.BrowserContext, error) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0",
+		"Mozilla/5.0 (X11; Linux i686; rv:147.0) Gecko/20100101 Firefox/147.0",
+	}
+
+	sizes := []playwright.Size{
+		{Width: 1280, Height: 720},
+		{Width: 1366, Height: 768},
+		{Width: 1920, Height: 1080},
+		{Width: 1440, Height: 900},
+	}
+
+	timezoneIds := []string{
+		"America/Belem",
+		"America/Sao_Paulo",
+	}
+
+	randomUserAgent := userAgents[r.Intn(len(userAgents))]
+	randomSize := sizes[r.Intn(len(sizes))]
+	randomTimezoneId := timezoneIds[r.Intn(len(timezoneIds))]
+
+	context, err := browser.NewContext(playwright.BrowserNewContextOptions{
+		UserAgent:  playwright.String(randomUserAgent),
+		Viewport:   &randomSize,
+		Locale:     playwright.String("pt-BR"),
+		TimezoneId: playwright.String(randomTimezoneId),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return context, nil
+}
+
 
 func main() {
 	pw, err := playwright.Run()
@@ -16,18 +62,13 @@ func main() {
 	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
 		//Headless: playwright.Bool(false),
 		Channel:  playwright.String("chrome"),
-		SlowMo:   playwright.Float(500),
+		SlowMo:   playwright.Float(200),
 	})
 	if err != nil {
 		log.Fatalf("could not launch browser: %v", err)
 	}
-	context, err := browser.NewContext(playwright.BrowserNewContextOptions{
-		UserAgent: playwright.String("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"),
-		Viewport: &playwright.Size{Width: 1280, Height: 720},
-		Locale:   playwright.String("pt-BR"),
-    	TimezoneId: playwright.String("America/Belem"),
-	})
 
+	context, err := generateContext(browser)
 
 	page, err := context.NewPage()
 	if err != nil {
